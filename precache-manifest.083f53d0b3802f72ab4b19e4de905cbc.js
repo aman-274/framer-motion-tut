81ffcaab559b94c187ab08e0f0b62040e7b33223
aman-275 <amandhamanda24@gmail.com>
2022-04-25 10:21:09 +0530

    Updates

---
+++ precache-manifest.083f53d0b3802f72ab4b19e4de905cbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "733275e7c675b402a9bb5b3cd6010649",
+    "url": "/framer-motion-tut/index.html"
+  },
+  {
+    "revision": "4dcf0eb2a6d3259f7181",
+    "url": "/framer-motion-tut/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "910e986824b657e3ac1f",
+    "url": "/framer-motion-tut/static/js/2.2bf190c3.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/framer-motion-tut/static/js/2.2bf190c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dcf0eb2a6d3259f7181",
+    "url": "/framer-motion-tut/static/js/main.c008f5a4.chunk.js"
+  },
+  {
+    "revision": "502b4409bfb76a5370b6",
+    "url": "/framer-motion-tut/static/js/runtime-main.7c5cdaa5.js"
+  }
+]);
