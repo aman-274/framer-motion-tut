33099bb88c3a8aa3d024e6a2cc3b8fdb465b7be3
aman-275 <amandhamanda24@gmail.com>
2022-04-25 10:58:55 +0530

    fix change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from "react";
 // import HomePage from './pages/HomePage';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import GlobalStyle from './globalStyle';
-import Navbar from './components/Navbar';
-import HomePage from './pages';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import GlobalStyle from "./globalStyle";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages";
 // import Footer from './components/Footer/Footer';
 
 function App() {
-	return (
-		<Router>
-			<GlobalStyle />
-			<Navbar/>
-			<Switch>
-				<Route path="/" exact component={HomePage} />
-			</Switch>
-			{/* <Footer /> */}
-		</Router>
-	);
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <GlobalStyle />
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+      </Switch>
+      {/* <Footer /> */}
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Clients/Clients.jsx
+++ src/components/Clients/Clients.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
+import React from "react";
 import {
-	ClientSection,
-	ClientColumn,
-	ClientText,
-	ClientTitle,
-	ClientRow,
-	ClientWrapper,
-	ClientImage,
-
-} from './ClientsStyles';
-import { clientsData } from '../../data/ClientsData';
+  ClientSection,
+  ClientColumn,
+  ClientText,
+  ClientTitle,
+  ClientRow,
+  ClientWrapper,
+  ClientImage,
+} from "./ClientsStyles";
+import { clientsData } from "../../data/ClientsData";
 
 export default function Clients() {
   return (
     <ClientSection id="clients">
-        <ClientTitle>
-            Our Clients
-        </ClientTitle>
-<ClientText>We’ve been working with the teams around the world.</ClientText>
-<ClientRow>
-					{clientsData.map((clients, clientsIndex) => (
-						<ClientColumn key={clientsIndex}>
-							{clients.map((el, index) => (
-								<ClientWrapper key={index}>
-									<ClientImage src={`./images/companies/${el.name}.svg`} />
-								</ClientWrapper>
-							))}
-						</ClientColumn>
-					))}
-				</ClientRow>
+      <ClientTitle>Our Clients</ClientTitle>
+      <ClientText>
+        We’ve been working with the teams around the world.
+      </ClientText>
+      <ClientRow>
+        {clientsData.map((clients, clientsIndex) => (
+          <ClientColumn key={clientsIndex}>
+            {clients.map((el, index) => (
+              <ClientWrapper key={index}>
+                <ClientImage
+                  src={`${process.env.PUBLIC_URL}/images/companies/${el.name}.svg`}
+                />
+              </ClientWrapper>
+            ))}
+          </ClientColumn>
+        ))}
+      </ClientRow>
     </ClientSection>
-  )
+  );
 }
